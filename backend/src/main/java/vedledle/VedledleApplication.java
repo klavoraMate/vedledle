3babd60d8bbb27ab6748a176613e14a4cce7a23c
klavoraMate <klavoramate98@gmail.com>
2023-08-05 14:45:54 +0200

    feat: created controller for testing reason

--- backend/src/main/java/vedledle/VedledleApplication.java
+++ backend/src/main/java/vedledle/VedledleApplication.java
@@ -2,11 +2,9 @@ package vedledle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class VedledleApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(VedledleApplication.class, args);
     }
--- backend/src/main/java/vedledle/controller/AuthenticationController.java
+++ backend/src/main/java/vedledle/controller/AuthenticationController.java
@@ -4,10 +4,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class AuthenticationController {
     @GetMapping("/")
     public ResponseEntity<String> root(){
         return ResponseEntity.ok("ok");
     }
+
 }
