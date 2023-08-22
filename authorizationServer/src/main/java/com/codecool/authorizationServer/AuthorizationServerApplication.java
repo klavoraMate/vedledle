a9ddc15b6526854e5e05a4ef67428e8162c50cc6
klavoraMate <klavoramate98@gmail.com>
2023-08-22 15:00:03 +0200

    feat: added inmemory user deatails

--- authorizationServer/src/main/java/com/codecool/authorizationServer/AuthorizationServerApplication.java
+++ authorizationServer/src/main/java/com/codecool/authorizationServer/AuthorizationServerApplication.java
@@ -2,6 +2,9 @@ package com.codecool.authorizationServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @SpringBootApplication
 public class AuthorizationServerApplication {
@@ -9,5 +12,8 @@ public class AuthorizationServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AuthorizationServerApplication.class, args);
 	}
-
+	@Bean
+	InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+		return new InMemoryUserDetailsManager(User.withDefaultPasswordEncoder().roles("admin").username("admin").password("pw").build());
+	}
 }
