8f709cbeb8b3c7bfc93d9f91cfab0b4f6c5e1f4f
klavoraMate <klavoramate98@gmail.com>
2023-08-22 15:00:26 +0200

    feat: moved pre authorized annotation to controller level

--- backend/src/main/java/vedledle/controller/AuthenticationController.java
+++ backend/src/main/java/vedledle/controller/AuthenticationController.java
@@ -2,6 +2,9 @@ package vedledle.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import vedledle.dao.model.Client;
@@ -12,9 +15,12 @@ import vedledle.service.ClientService;
 @RequiredArgsConstructor
 public class AuthenticationController {
     private final ClientService service;
+
+    @PreAuthorize("hasAuthority('SCOPE_user.read')")
     @GetMapping("/")
     public String root(){
-        return service.get(1L).getName();
+        var jwt = (Jwt) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        return jwt.getIssuer() + service.get(1L).getName();
     }
 
 }
