6f4b772316cb411f9ae41e095b64c007e135f97a
klavoraMate <klavoramate98@gmail.com>
2023-09-11 11:13:51 +0200

    feat: added AppBar created by material ui

---
+++ fronted/src/app/login/page.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+import AppBar from "@/_components/AppBar";
+import {FormControl, InputLabel} from "@mui/material";
+import React from "react";
+
+export default function Login() {
+    return (
+        <div>
+            <AppBar>
+            </AppBar>
+            <FormControl>
+            </FormControl>
+        </div>
+    )
+}
