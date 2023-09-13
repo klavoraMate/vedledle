a5d8692e59098c7c19e2b03c4221b1639c880828
klavoraMate <klavoramate98@gmail.com>
2023-09-13 13:19:00 +0200

    feat: created login endpoint as a authentication entrypoint
    I have removed JWTGenerator filter and replaced with JWTGenerator class because
    now im initatig the authentication flow from the /login endpoint in order to get rid of formLogin()

--- backend/src/main/java/vedledle/config/security/SecurityConfiguration.java
+++ backend/src/main/java/vedledle/config/security/SecurityConfiguration.java
@@ -4,29 +4,23 @@ package vedledle.config.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-import vedledle.config.security.filter.JWTTokenGeneratorFilter;
-import vedledle.config.security.filter.JWTTokenValidatorFilter;
+import vedledle.config.security.filter.JWTValidatorFilter;
 
 @Configuration
 public class SecurityConfiguration {
-
     @Bean
     public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
-                .addFilterBefore(new JWTTokenGeneratorFilter(), BasicAuthenticationFilter.class)
-                .addFilterAfter(new JWTTokenValidatorFilter(), BasicAuthenticationFilter.class)
+                .addFilterAfter(new JWTValidatorFilter(), BasicAuthenticationFilter.class)
                 .authorizeHttpRequests()
                 .requestMatchers("/login","/register").permitAll()
                 .requestMatchers("/user").hasRole("USER")
-                .anyRequest().authenticated()
-                .and()
-                .formLogin(Customizer.withDefaults());
+                .anyRequest().authenticated();
         return http.build();
     }
 
--- backend/src/main/java/vedledle/controller/AuthenticationController.java
+++ backend/src/main/java/vedledle/controller/AuthenticationController.java
@@ -3,13 +3,18 @@ package vedledle.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
+import vedledle.config.security.JWTGenerator;
+import vedledle.config.security.provider.UsernamePasswordAuthenticationProvider;
+import vedledle.controller.dto.LoginRequest;
+import vedledle.controller.dto.LoginResponse;
 import vedledle.dao.model.Client;
 import vedledle.service.ClientService;
 
-import java.util.List;
 import java.util.Optional;
 
 
@@ -18,6 +23,8 @@ import java.util.Optional;
 public class AuthenticationController {
     private final PasswordEncoder passwordEncoder;
     private final ClientService service;
+    private final UsernamePasswordAuthenticationProvider authenticationProvider;
+
 
     @RequestMapping("/user")
     public Client getUserDetailsAfterLogin(Authentication authentication) {
@@ -46,6 +53,17 @@ public class AuthenticationController {
                     .body("An exception occurred due to " + e.getMessage());
         }
         return response;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest loginRequest) {
+        try {
+            Authentication authentication = authenticationProvider.authenticate(new UsernamePasswordAuthenticationToken(loginRequest.username(), loginRequest.password()));
+            String jwt = JWTGenerator.generate(authentication);
+            return ResponseEntity.ok(new LoginResponse(jwt));
+        } catch (Exception e) {
+            throw new BadCredentialsException(e.getMessage());
+        }
     }
 
 }
