0e88b9bc9d7eddb5d8fd6399d7b90f9098ebd21d
klavoraMate <klavoramate98@gmail.com>
2023-09-25 14:14:49 +0200

    fix: collumn error

--- backend/src/main/java/vedledle/dao/model/User.java
+++ backend/src/main/java/vedledle/dao/model/User.java
@@ -10,6 +10,7 @@ import lombok.*;
 @Builder
 @NoArgsConstructor(force = true)
 @AllArgsConstructor
+@Table(name = "client")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
