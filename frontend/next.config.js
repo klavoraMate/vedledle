bccf78152fe32b41b70f1f2bfd33c9f3bb575b0f
klavoraMate <klavoramate98@gmail.com>
2023-09-27 16:27:58 +0200

    refactor: changed to docker enviroment

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async rewrites() {
-        return [
-            {
-                source: '/api/:path*',
-                destination: 'http://127.0.0.1:8080/api/:path*',
-            },
-        ]
-    },
+    output : "export"
 }
 
 module.exports = nextConfig
