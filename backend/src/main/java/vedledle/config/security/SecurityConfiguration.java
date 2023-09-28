bed444eb12516591817e05a756c5176f75372652
klavoraMate <klavoramate98@gmail.com>
2023-09-28 09:26:43 +0200

    fix: fixed protected endpoints which were  not set correctly

--- backend/src/main/java/vedledle/config/security/SecurityConfiguration.java
+++ backend/src/main/java/vedledle/config/security/SecurityConfiguration.java
@@ -28,7 +28,8 @@ public class SecurityConfiguration {
                         "/user",
                         "/api/login",
                         "/api/register").permitAll()
-                .requestMatchers("/api/dog/**,/api/user/**").hasRole("USER")
+                .requestMatchers("/api/dog/**"
+                        ,"/api/user/**").hasRole("USER")
                 .anyRequest().authenticated();
         return http.build();
     }
