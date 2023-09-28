a900d3a3c77706b9be5e251668bfba2a67c81690
klavoraMate <klavoramate98@gmail.com>
2023-09-28 13:15:32 +0200

    feat: added registration page

--- backend/src/main/java/vedledle/config/security/SecurityConfiguration.java
+++ backend/src/main/java/vedledle/config/security/SecurityConfiguration.java
@@ -25,6 +25,7 @@ public class SecurityConfiguration {
                         "/*.css",
                         "/*.png",
                         "/login",
+                        "/register",
                         "/profile",
                         "/api/login",
                         "/api/register").permitAll()
