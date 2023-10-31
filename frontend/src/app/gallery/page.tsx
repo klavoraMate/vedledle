4e0715381061d6b3c1c423f66c89390fa73e6ccd
klavoraMate <klavoramate98@gmail.com>
2023-10-31 17:39:20 +0100

    feat: upload button only shows when admin user logged in

--- frontend/src/app/gallery/page.tsx
+++ frontend/src/app/gallery/page.tsx
@@ -5,37 +5,40 @@ import {ImageList, ImageListItem} from "@mui/material";
 import {useEffect, useState} from "react";
 import GalleryImage from "@/app/gallery/GalleryImage";
 import GalleryUploadButton from "@/app/gallery/GalleryUploadButton";
+import {getRole} from "@/util/JWTDecoder"
 
-export default function Gallery(){
-    const [imageNames,setImageNames] = useState<string[]>([]);
+export default function Gallery() {
+    const [imageNames, setImageNames] = useState<string[]>([]);
+    const role = getRole();
+
     async function fetchNames() {
         try {
-            const response = await fetch('/api/image/names',{
-                method:"GET"
+            const response = await fetch('/api/image/names', {
+                method: "GET"
             })
-            if (response.ok){
-                const names : string[] = await  response.json();
+            if (response.ok) {
+                const names: string[] = await response.json();
                 setImageNames(names)
             }
-        }catch (e) {
-            console.error("Error during fetching image names: ",e)
+        } catch (e) {
+            console.error("Error during fetching image names: ", e)
         }
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         fetchNames();
-    },[]);
-    return(
+    }, []);
+    return (
         <Layout>
-            <Box sx={{overflowY:'scroll'}}>
+            <Box sx={{overflowY: 'scroll'}}>
                 <ImageList variant="masonry" cols={3} gap={8}>
-                    {imageNames && imageNames.map((name,index) => (
+                    {imageNames && imageNames.map((name, index) => (
                         <ImageListItem key={index}>
                             <GalleryImage imageName={name}/>
                         </ImageListItem>
                     ))}
                 </ImageList>
-                <GalleryUploadButton/>
+                {role === 'ADMIN' && <GalleryUploadButton/>}
             </Box>
         </Layout>
     )
