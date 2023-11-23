db997df408c865c372240e190a1a2ea933c83804
klavoraMate <klavoramate98@gmail.com>
2023-11-23 12:14:28 +0100

    feat: change  profile page structure
    Profile page has beed reduced and moved into different components: DogCardContainer,DogCard and UserInfoCard
    Now the fetching of the information is different too. Dogs are fetched based on an email and not coming from the backend.

---
+++ frontend/src/app/profile/profile_component/UserInfoCard.tsx
@@ -0,0 +1,79 @@
+import React, {useEffect, useState} from 'react';
+import {Card, CardContent, Typography} from '@mui/material';
+import {makeStyles} from '@material-ui/core/styles';
+import {UserData} from "@/util/types";
+import {getEmail, getJWT} from "@/util/JWTDecoder";
+
+export default function UserInfoCard() {
+    const email = getEmail();
+    const jwt = getJWT();
+    const classes = useStyles();
+    const [loading, setLoading] = useState(true);
+    const [userData, setUserData] = useState<UserData | null>(null);
+
+    const fetchUserData = async () => {
+        try {
+            if (email) {
+                setLoading(true);
+                const response = await fetch(`/api/user/info?email=${encodeURIComponent(email.toString())}`, {
+                    method: 'GET',
+                    headers: {
+                        Authorization: `Bearer ${jwt}`,
+                    },
+                });
+
+                if (response.ok) {
+                    const userData: UserData = await response.json();
+                    setUserData(userData);
+                    setLoading(false);
+                }
+            }
+        } catch (error) {
+            console.error('Error fetching user data:', error);
+            setLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        if (email && jwt) {
+            fetchUserData();
+        }
+    }, [email, jwt]);
+
+    return (
+        <>
+            {!loading ?
+                (<Card className={classes.card}>
+                    <CardContent>
+                        <Typography className={classes.title} gutterBottom>
+                            User Information
+                        </Typography>
+                        <Typography variant="body1">
+                            <strong>Name:</strong> {userData?.name}
+                        </Typography>
+                        <Typography variant="body1">
+                            <strong>Email:</strong> {userData?.email}
+                        </Typography>
+                    </CardContent>
+                </Card>) :
+                (<Card className={classes.card}>
+                    <CardContent>
+                        <Typography className={classes.title} gutterBottom>
+                            Loading ..
+                        </Typography>
+                    </CardContent>
+                </Card>)
+            }
+        </>
+    );
+};
+
+const useStyles = makeStyles((theme) => ({
+    card: {
+        minWidth: 275,
+        marginBottom: theme.spacing(2),
+    },
+    title: {
+        fontSize: 20,
+    },
+}));
---
+++ frontend/src/util/types.ts
@@ -0,0 +1,12 @@
+
+export type DogData = {
+    name: string;
+    breed: string;
+    size: string;
+    age: number;
+};
+
+export type UserData = {
+    name: string;
+    email: string;
+};
