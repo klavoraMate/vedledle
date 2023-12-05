6281d36bd9ede427dc442277aab602296a2a4b37
klavoraMate <klavoramate98@gmail.com>
2023-12-05 10:20:09 +0100

    feat: added JPA repository for the Reservation entity

---
+++ backend/src/main/java/vedledle/dao/repository/ReservationRepository.java
@@ -0,0 +1,8 @@
+package vedledle.dao.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import vedledle.dao.model.Reservation;
+
+public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+    
+}
